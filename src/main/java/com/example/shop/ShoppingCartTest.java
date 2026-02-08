178e11d75e127b5ceecf3252f16004563852c098
Adam Majava <vonadamo@gmail.com>
2026-02-08 22:05:23 +0100

    Add ShoppingCart class and initial test for empty cart total price

---
+++ src/main/java/com/example/shop/ShoppingCartTest.java
@@ -0,0 +1,11 @@
+package com.example.shop;
+
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    void totalPriceIsZeroForEmptyCart() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+    }
+}
